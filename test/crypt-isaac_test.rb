dd4b54041e718963a8d786a9fa472ad023a432b0
beawesomeinstead <beawesomeinstead@yahoo.com>
2009-07-07 13:14:47 +0300

    Documentation and whitespace fixes.

--- test/crypt-isaac_test.rb
+++ test/crypt-isaac_test.rb
@@ -8,24 +8,24 @@ class CryptIsaacTest < Test::Unit::TestCase
   end
 
   def test_kind
-    assert_kind_of(Crypt::ISAAC,@generator,"The created object is not a Crypt::ISAAC or subclass thereof.") 
+    assert_kind_of(Crypt::ISAAC, @generator, "The created object is not a Crypt::ISAAC or subclass thereof.")
   end
 
   def test_integer
     assert_nothing_raised("Failed while generating an integer random number.") do
       mynum = @generator.rand(1000000)
-      assert_kind_of(Integer,mynum,"The generator failed to return an integer number in response to @generator.rand(1000000).")
-      assert((mynum >= 0),"The generator returned a number that is less than 0 (#{mynum}).")
-      assert((mynum < 1000000),"The generator returned a number that is greater than or equal to 1000000 (#{mynum}).")
+      assert_kind_of(Integer, mynum, "The generator failed to return an integer number in response to @generator.rand(1000000).")
+      assert((mynum >= 0), "The generator returned a number that is less than 0 (#{mynum}).")
+      assert((mynum < 1000000), "The generator returned a number that is greater than or equal to 1000000 (#{mynum}).")
     end
   end
 
   def test_float
     assert_nothing_raised("Failed while generating a floating point random number.") do
       mynum = @generator.rand()
-      assert_kind_of(Float,mynum,"The generator failed to return a floating point number in response to @generator.rand().")
-      assert((mynum >= 0),"The generator returned a number that is less than 0 (#{mynum}).")
-      assert((mynum < 1),"The generator returned a number that is greater than or equal to 1 (#{mynum}).")
+      assert_kind_of(Float, mynum, "The generator failed to return a floating point number in response to @generator.rand().")
+      assert((mynum >= 0), "The generator returned a number that is less than 0 (#{mynum}).")
+      assert((mynum < 1), "The generator returned a number that is greater than or equal to 1 (#{mynum}).")
     end
   end
 
@@ -53,7 +53,7 @@ class CryptIsaacTest < Test::Unit::TestCase
     assert_nothing_raised("Failed to pull numbers from two independent streams.") do
       g1 = Crypt::ISAAC.new
       g2 = Crypt::ISAAC.new
-      assert((g1 != g2),"The generators are the same.  This should not happen.")
+      assert((g1 != g2),"The generators are the same. This should not happen.")
       1000.times do
         g1.rand(4294967295)
         g2.rand(4294967295)
